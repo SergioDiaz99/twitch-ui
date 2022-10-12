ac19949979e25f6a73df848d65a23274f66ac815
unknown <sergiodiaz@latechfactory.com>
2022-10-12 03:10:53 -0300

    Twitch UI clone - Sergio Diaz

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -27,6 +27,7 @@ const Navbar = () => {
   return (
     <div className="fixed z-[100] h-14 w-full flex flex-nowrap items-center p-4 bg-primary mb-[2px]">
       <div className="flex grow items-center justify-start">
+      asdsad
         <Link href="/">
           <a className="flex">
             {/*eslint-disable-next-line @next/next/no-img-element*/}
